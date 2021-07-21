584344ebc6d4e37056c0f24973721e1801b2dfa8
chandresh Patidar <patidarchandresh2002@gmail.com>
2021-07-21 10:49:39 +0530

    added project section

--- js/script.js
+++ js/script.js
@@ -12,14 +12,17 @@ window.onscroll = function (e) {
     const header = document.getElementsByTagName('header')[0];
     const logo = document.getElementsByClassName('logo')[0];
     const menu_bar  =document.getElementsByClassName('line');
+    const nav_link = document.getElementsByClassName('nav_link');
     if (window.scrollY !== 0 ){
          header.style.background = "var(--primary_background)"; 
          logo.style.color = 'var(--white_font)';
          Array.from(menu_bar).forEach(ele => ele.style.stroke = 'var(--white_font)')
+         Array.from(nav_link).forEach(ele => ele.style.color = 'var(--white_font)')
         }
     else{
         header.style.background = "transparent";
         logo.style.color = 'var(--primary_background)';  
         Array.from(menu_bar).forEach(ele => ele.style.stroke = 'var(--primary_background)')
+        Array.from(nav_link).forEach(ele => ele.style.color = 'var(--primary_background)')
        }
 }
